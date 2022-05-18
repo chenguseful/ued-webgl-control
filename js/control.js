f4a65c1aa2c1a5bcb9da8a2b41f391fc96ff3c46
cheng.yan <cheng.yan@ustcinfo.com>
2022-05-18 10:16:09 +0800

    VR按钮测试

--- js/control.js
+++ js/control.js
@@ -152,7 +152,7 @@ function onSelectStart(event) {
     if (intersections.length > 0) {
         var intersection = intersections[0];
         if (selected.length > 0) {
-            selected[0].material.color.r = 48;
+            selected[0].material.color.r = 48 / 255;
             selected = []
         }
         var object = intersection.object;
@@ -169,7 +169,7 @@ function onSelectEnd(event) {
         var object = controller.userData.selected;
         // object.material.emissive.b = 0;
         // object.material.transparent = true
-        object.material.color.r = 255;
+        object.material.color.r = 255 / 255;
         // group.attach(object);
         controller.userData.selected = undefined;
     }
